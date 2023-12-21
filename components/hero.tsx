432b92e02f4b1475fa01a9984a300f73db86b5c2
amar-eng <am24mo@gmail.com>
2023-12-21 13:18:19 -0500

    changes

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,18 +1,18 @@
 'use client';
-import getCategories from '@/actions/get-categories';
+// import getCategories from '@/actions/get-categories';
 import BookBtn from './bookBtn';
 import { Button } from './ui/button';
-import getServices from '@/actions/get-services';
+// import getServices from '@/actions/get-services';
 
 const Hero = async () => {
-  const categories = await getCategories();
+  // const categories = await getCategories();
 
-  const wantedCategory = categories.find(
-    (category) => category.name === 'Home'
-  );
-  const services = await getServices({
-    categoryId: wantedCategory?.id,
-  });
+  // const wantedCategory = categories.find(
+  //   (category) => category.name === 'Home'
+  // );
+  // const services = await getServices({
+  //   categoryId: wantedCategory?.id,
+  // });
   return (
     <>
       <div className=" h-[70vh] lg:h-[75vh] relative z-10 px-4  text-white  flex flex-col justify-center items-center">
@@ -21,7 +21,7 @@ const Hero = async () => {
           Connect with vetted professional cleaners for your space. No
           contracts, no risk, and affordable prices.
         </p>
-        {services && services.map((cat) => <p>{cat.name}</p>)}
+        {/* {services && services.map((cat) => <p>{cat.name}</p>)} */}
         <div className="flex justify-between items-start  w-100">
           <BookBtn />
           <Button className="mx-5 w-50 py-6" size="lg" variant="ghost">
